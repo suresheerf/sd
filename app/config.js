c17c1e7b9e6a7acbb41a4321cff30a80fb7eb408
suresh d <suresheerf@gmail.com>
2024-01-21 16:55:20 +0530

    updated

--- app/config.js
+++ app/config.js
@@ -9,3 +9,6 @@ export const PORT = process.env.PORT || 3000;
 
 export const DBURI = process.env.DBURI || 'mongodb://localhost:27017/sd';
 
+export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
