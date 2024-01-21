9903ff746e76f13564a78409be034f58ce01f243
suresh d <suresheerf@gmail.com>
2024-01-21 16:54:54 +0530

    import fix

--- app/auth/service.js
+++ app/auth/service.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/config.js';
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../config.js';
 export default (data, organizationId) =>
   jwt.sign(data, JWT_SECRET, {
     expiresIn: JWT_EXPIRES_IN,
